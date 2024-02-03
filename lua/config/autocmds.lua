d737ba46148b4c2fca35750782a67b01231cb453
David Voswinkel <david@userlike.com>
2024-02-03 15:03:03 +0100

    Add colorscheme and first plugins

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,31 @@
 -- Autocmds are automatically loaded on the VeryLazy event
+--
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("highlight_yank"),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile", "BufEnter" }, {
+  group = vim.api.nvim_create_augroup("Ansible", { clear = true }),
+  pattern = {
+    "*/roles/*/*/.yml",
+    "main.yml",
+    "*/ansible/*.yml",
+    "group_vars/*.yml",
+    "host_vars/*.yml",
+    "update_*.yml",
+  },
+  callback = function()
+    vim.opt.filetype = "yaml.ansible"
+  end,
+})
